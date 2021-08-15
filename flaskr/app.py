dd5a3f5579cb7e884cb88764a004ad425bd8bd3e
Lino Melhado <linomneto@gmail.com>
2021-08-15 11:08:02 -0300

    adjusting project direcoty structure and implementing Calculator class

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
 #!python3
 
 from flask import Flask, request, render_template
+from classes.Calculator import Calculator
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -11,6 +12,11 @@ def index():
 
 @app.route('/calculate', methods=["POST"])
 def calculate():
-    return "calculate method"
+    n1 = float(request.form.get("n1"))
+    n2 = float(request.form.get("n2"))
+    op = request.form.get("op")
+    calc = Calculator(n1, op, n2)
+    calc.run()
+    return str(calc.result)
 
 app.run()
---
+++ flaskr/classes/Calculator.py
@@ -0,0 +1,22 @@
+#!python3
+
+class Calculator:
+    
+    def __init__(self, n1, op, n2):
+        self.n1 = n1
+        self.op = op
+        self.n2 = n2
+
+    def run(self):
+        if (self.op == '+'):
+            self.result = self.n1 + self.n2
+        elif (self.op == '-'):
+            self.result = self.n1 - self.n2
+        elif (self.op == '/'):
+            self.result = self.n1 / self.n2
+        elif (self.op == '*'):
+            self.result = self.n1 * self.n2
+        else:
+            raise Exception("Not support calculation operation.")
+        return self.result
+    
